2045c88aa8562f0d6687fe393c4b6ef201c9bf24
Natalia <natali.yarosh.96@mail.ru>
2019-09-01 15:03:20 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -49,5 +49,18 @@ $(function(){
         nav.toggleClass("show");
     })
 
+    /*Reviews: https://kenwheeler.github.io/slick/*/
+
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
 
 });
