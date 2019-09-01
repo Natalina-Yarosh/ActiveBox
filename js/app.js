45f5e898a9658315297319f2b5b046d00d9c43bd
Natalia <natali.yarosh.96@mail.ru>
2019-09-01 14:03:22 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ $(function(){
     let introH= intro.innerHeight();;
     let scrollPos = $(window).scrollTop();
 
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
     checkScroll(scrollPos, introH);
 
     $(window).on("scroll resize", function(){
@@ -31,11 +34,20 @@ $(function(){
         let elementId = $(this).data('scroll');
         let elementOffset = $(elementId).offset().top;
 
+        nav.removeClass("show");
+
         $("html, body").animate({
             scrollTop: elementOffset - 70
         }, 700);
     })
   
+    /*Nav Toggle*/
+    
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    })
 
 
 });
